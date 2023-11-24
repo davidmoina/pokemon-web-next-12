99c55139ac01ef876749487c1139b6e5ddc1e54c
davidmoina <luisdavidmoina@gmail.com>
2023-11-25 00:47:36 +0100

    🔍 Add open graph meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { Navbar } from "../ui";
+import { useRouter } from "next/router";
 
 interface Props {
   children: JSX.Element | JSX.Element[];
   title?: string;
 }
+
+const origin = typeof window !== "undefined" ? window.location.origin : "";
 
 export const Layout = ({ children, title }: Props) => {
   return (
@@ -17,6 +20,15 @@ export const Layout = ({ children, title }: Props) => {
           content={`Información sobre el pokémon ${title}`}
         />
         <meta name="keywords" content={`${title}, pokemon, pokedex, pokeapi`} />
+        <meta
+          property="og:title"
+          content={`Informacion sobre el pokémon ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`Esta es la pagina sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main className="px-3">{children}</main>
